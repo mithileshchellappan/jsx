c8666716f31bfd87ab18a7a5fb106e806c9139d6
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-04-03 14:33:05 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import react & react dom libs
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function getButtText(){
+    return 'Click me';
+}
+
+//create react component
+const App = () => {
+
+    const text1 = 'click kar'
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {getButtText()}
+      </button>
+    </div>
+  );
+};
+//take react comp and show on screen
+ReactDOM.render(<App />, document.querySelector("#root"));
