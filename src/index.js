36aebd20b8ada16797e40a86d666a84080115867
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-04-29 19:51:13 +0530

    components init

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from './seasonDisplay';
-import Spinner from './Spinner';
-class App extends React.Component {
-  state={lat:null,errMess:''};
+import App from "./components/App";
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) =>this.setState({lat: position.coords.latitude,}),
-      (err) =>this.setState({errMess: err.message,})
-    );
-  }
-
-  render() {
-    if (this.state.errMess === "" && this.state.lat == null) {
-      return <Spinner message="Accept or die"/>;
-    } else {
-      if (this.state.errMess === "") {
-        return <SeasonDisplay lat={this.state.lat}/>;
-      } else {
-        return <div>Err {this.state.errMess}</div>;
-      }
-    }
-  }
-}
 ReactDOM.render(<App />, document.querySelector("#root"));
