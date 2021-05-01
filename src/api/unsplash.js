7e320d3d07b0feed7c370db5132826f91e2a71d6
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-05-01 12:02:03 +0530

    yt clone init

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL:'https://api.unsplash.com',
-    headers: {
-        Authorization: "Client-ID CbRa-gD5D1VsofD6kbxHCNQGQ7xpmGMqiv3AZs6sMNw",
-      },
-});
